0eacbf2b7cd9ee660971b1043b65e062130da50f
Arnel T. Lenteria <arnel.lenteria@gmail.com>
2016-11-14 16:10:28 +0800

    PostageApp.configure do |config|   if Rails.env.production?     config.api_key = 'I77xvjfjsjN975dlb0iyfYdBBNQNXTFK'   else     config.api_key = 'UJoGsYo4Oxs0dJfR036B7yZdefHLybTe'   end   config.max_retry = 5 end

--- lib/postageapp/failed_request.rb
+++ lib/postageapp/failed_request.rb
@@ -27,43 +27,61 @@ module PostageApp::FailedRequest
   
   # Attempting to resend failed requests
   def self.resend_all
-    return false unless (self.store_path)
-    
+    return false if !store_path
     Dir.foreach(store_path) do |filename|
-      next unless (filename.match(/^\w{40}$/))
-      
+      next if !filename.match /^\w{40}$/
+
       request = initialize_request(filename)
-      
-      receipt_response = PostageApp::Request.new(
-        :get_message_receipt,
-        uid: filename
-      ).send(true)
 
-      if (receipt_response.fail?)
+      next if reached_max_retries?(request)
+
+      receipt_response = PostageApp::Request.new(:get_message_receipt, :uid => filename).send(true)
+      log_retries(request)
+
+      if receipt_response.fail?
         return
-      elsif (receipt_response.ok?)
-        PostageApp.logger.info("Skipping failed request (already sent) [#{filename}]")
+      elsif receipt_response.ok?
+        PostageApp.logger.info "NOT RESENDING FAILED REQUEST [#{filename}]"
+        File.delete(file_path(filename)) rescue nil
 
-        force_delete!(file_path(filename))
-      elsif (receipt_response.not_found?)
-        PostageApp.logger.info("Retrying failed request [#{filename}]")
-
+      elsif receipt_response.not_found? && !reached_max_retries?(request)
+        PostageApp.logger.info "RESENDING FAILED REQUEST [#{filename}]"
         response = request.send(true)
-        
+        log_retries(request)
         # Not a fail, so we can remove this file, if it was then
         # there will be another attempt to resend
-
-        unless (response.fail?)
-          force_delete!(file_path(filename))
-        end
+        File.delete(file_path(filename)) rescue nil if !response.fail?
       else
-        PostageApp.logger.info("Skipping failed request (non-replayable request type) [#{filename}]")
-
-        force_delete!(file_path(filename))
+        PostageApp.logger.info "NOT RESENDING FAILED REQUEST [#{filename}], RECEIPT CANNOT BE PROCESSED"
+        File.delete(file_path(filename)) rescue nil
       end
     end
-
     return
+  end
+
+  def self.reached_max_retries?(request)
+    retries[request.uid].to_i >= PostageApp.config.max_retry
+  rescue
+    false
+  end
+
+  def self.log_retries(request)
+    _retries = retries
+    _retries[request.uid] = _retries[request.uid].to_i + 1
+    File.open(retries_log_file, 'w+') do |f|
+      f.write _retries.to_yaml
+    end
+  end
+
+  def self.retries
+    YAML.load_file(retries_log_file)
+  rescue
+    {}
+  end
+
+  def self.retries_log_file
+    FileUtils.mkdir_p(File.join(File.expand_path(PostageApp.configuration.project_root), 'tmp'))
+    File.join(File.join(PostageApp.configuration.project_root, "tmp/postageapp_retries.yaml"))
   end
   
   # Initializing PostageApp::Request object from the file
